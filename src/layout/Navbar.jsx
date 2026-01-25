d1e80f2455a77e000d0a0f9ff272222c72e8e2c2
lu <a0960753785@gmail.com>
2026-01-25 23:25:40 +0800

    完成手機板navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,86 +1,266 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-function Offcanvas({ isOpen, close }) {
-    return (<div className={`offcanvas offcanvas-top h-100 bg-primary-10 ${isOpen ? 'show' : ''}`}
-        tabIndex="-1"
-        style={{
-            visibility: isOpen ? 'visible' : 'hidden',
-        }}>
-        <div className="d-flex justify-content-end p-4">
-            <button className="rounded-circle bg-white p-5  border-0" onClick={close}><i class="bi bi-x nav-icon"></i></button>
+function Offcanvas({ isOpen, close, type }) {
+  return (
+    <div
+      className={`offcanvas offcanvas-top h-100 ${
+        type === 'search' ? 'bg-primary-90 bg-opacity-75' : 'bg-primary-10'
+      }  ${isOpen ? 'show' : ''}`}
+      style={{ visibility: isOpen ? 'visible' : 'hidden' }}
+    >
+      <div
+        className={`justify-content-end p-4 ${
+          type === 'search' ? 'd-none' : 'd-flex'
+        }`}
+      >
+        <button
+          className="rounded-circle bg-white p-5  border-0"
+          onClick={close}
+        >
+          <i className="bi bi-x nav-icon"></i>
+        </button>
+      </div>
+      {type === 'menu' && <MenuContent />}
+      {type === 'search' && <SearchContent close={close} />}
+    </div>
+  );
+}
+// 選單
+function MenuContent() {
+  return (
+    <>
+      <div className="offcanvas-body py-5 px-4">
+        <ul className="navbar-nav justify-content-center align-items-center flex-grow-1 mb-8 border-bottom border-primary-30">
+          <li className="nav-item pb-2">
+            <a className="nav-link fs-6 text-black px-5 py-4" href="#">
+              商品系列
+            </a>
+          </li>
+          <li className="nav-item pb-2">
+            <a className="nav-link fs-6 text-black px-5 py-4" href="#">
+              風格嚴選
+            </a>
+          </li>
+          <li className="nav-item pb-2">
+            <a className="nav-link fs-6 text-black px-5 py-4" href="#">
+              關於收購
+            </a>
+          </li>
+          <li className="nav-item pb-2">
+            <a className="nav-link fs-6 text-black px-5 py-4" href="#">
+              品牌故事
+            </a>
+          </li>
+          <li className="nav-item pb-2">
+            <a className="nav-link fs-6 text-black px-5 py-4" href="#">
+              連絡我們
+            </a>
+          </li>
+          <li className="nav-item pb-5">
+            <a className="nav-link fs-6 text-black px-5 py-4" href="#">
+              查看購物車
+            </a>
+          </li>
+        </ul>
+        <div className="d-flex justify-content-center align-items-center">
+          <a
+            href="#"
+            className="btn btn-outline-primary py-4 px-8 me-4 rounded-5"
+          >
+            註冊
+          </a>
+          <a href="#" className="btn btn-primary py-4 px-8 me-4 rounded-5">
+            登入
+          </a>
         </div>
-        <div className="offcanvas-body py-5 px-4">
-            <ul className="navbar-nav justify-content-center align-items-center flex-grow-1">
-                <li className="nav-item"><a className="nav-link fs-2" href="#">商品系列</a></li>
-                <li className="nav-item"><a className="nav-link fs-2" href="#">風格嚴選</a></li>
-                <li className="nav-item"><a className="nav-link fs-2" href="#">關於收購</a></li>
-                <li className="nav-item"><a className="nav-link fs-2" href="#">品牌故事</a></li>
-                <li className="nav-item"><a className="nav-link fs-2" href="#">連絡我們</a></li>
-                <li className="nav-item"><a className="nav-link fs-2" href="#">查看購物車</a></li>
-            </ul>
+      </div>
+      <div className="offcanvas-footer text-center mb-5">
+        <img src="./src/assets/images/logo/logo v2.svg" alt="logo" />
+      </div>
+    </>
+  );
+}
+// 搜尋
+function SearchContent({ close }) {
+  return (
+    <div className="offcanvas-body d-flex flex-column justify-content-center ps-8 pe-4">
+      <div className="d-flex">
+        <input
+          typeof="search"
+          className="search-input"
+          placeholder="搜尋"
+          autoFocus
+        />
+        <div className="icon-wrapper p-5">
+          <div
+            className="bg-transparent border-0 shadow-none p-0"
+            role="button"
+          >
+            <i className="bi bi-search text-white nav-icon"></i>
+          </div>
         </div>
-    </div>)
-};
+        <button
+          className="bg-transparent p-5 border-0 icon-wrapper"
+          onClick={close}
+        >
+          <i className="bi bi-x nav-icon text-white"></i>
+        </button>
+      </div>
+    </div>
+  );
+}
 
 function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggleOpen = () => {
-        setIsOpen(!isOpen);
+  // 手機板-漢堡選單、搜尋選單
+  const [isOpen, setIsOpen] = useState(false);
+  const [navType, setNavType] = useState('');
+  // 監聽滾輪
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  // 監聽滾輪
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50); // 50px 就觸發
     };
 
-    return (
-        <>
-            {/* 手機版導覽列 */}
-            <nav className="navbar navbar-light fixed-top d-lg-none p-4">
-                <div className="container-fluid align-items-start p-0">
-                    <a className="" href="#">
-                        <img src="./src/assets/images/logo/logo v2_white.svg" alt="logo" />
-                    </a>
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
-                    <div className="d-flex">
+  return (
+    <>
+      {/* 手機版導覽列 */}
+      <nav
+        className={`navbar navbar-light fixed-top d-lg-none p-4
+    ${isScrolled ? 'bg-primary-90 bg-opacity-75' : ''}  `}
+      >
+        <div className="container-fluid align-items-start p-0">
+          <a className="" href="#">
+            <img
+              src="./src/assets/images/logo/logo v2_white.svg"
+              alt="logo"
+              className={`logo-img ${isScrolled ? 'logo-small' : ''}`}
+            />
+          </a>
 
-                        <div className="me-3 p-5">
-                            <i className="bi bi-search text-white nav-icon "></i>
-                        </div>
+          <div className="d-flex">
+            <div className="me-3 p-5">
+              {/* 搜尋按鈕 */}
+              <div
+                className="bg-transparent border-0 shadow-none p-0"
+                role="button"
+                onClick={() => {
+                  setNavType('search');
+                  setIsOpen(true);
+                }}
+              >
+                <i className="bi bi-search text-white nav-icon"></i>
+              </div>
+            </div>
 
-                        {/* 漢堡按鈕：手動綁定 isOpen 類別 */}
-                        <button
-                            className={`navbar-toggler rounded-circle bg-white p-5  border-0  shadow-none
-wh-56 ${isOpen ? 'open' : ''}`}
-                            type="button"
-                            onClick={toggleOpen}
-                        >
-                            <div className="hamburger-icon ">
-                                <span className="line"></span>
-                                <span className="line middle"></span>
-                                <span className="line"></span>
-                            </div>
-                        </button>
-                    </div>
+            {/* 漢堡選單 */}
+            <button
+              className="navbar-toggler rounded-circle bg-white p-5  border-0  shadow-none wh-56"
+              type="button"
+              onClick={() => {
+                setNavType('menu');
+                setIsOpen(true);
+              }}
+            >
+              <div className="hamburger-icon ">
+                <span className="line"></span>
+                <span className="line middle"></span>
+                <span className="line"></span>
+              </div>
+            </button>
+          </div>
 
-                    <Offcanvas isOpen={isOpen} close={toggleOpen} />
+          <Offcanvas
+            isOpen={isOpen}
+            close={() => setIsOpen(false)}
+            type={navType}
+          />
+        </div>
+      </nav>
+
+      {/* 電腦版導覽列 */}
+      <div className="d-none d-lg-flex fixed-top">
+        <div className="">
+          <a className="" href="#">
+            <img
+              src="./src/assets/images/logo/logo v2_white.svg"
+              alt="logo"
+              className={`logo-img ${isScrolled ? 'logo-small' : ''}`}
+            />
+          </a>
+        </div>
+
+        <div className="bg-primary-90 h-100 px-12 py-25 rounded-start-bottom">
+          <div className="d-flex">
+            <ul className="d-flex me-auto mb-2 mb-lg-0">
+              <li className="p-5 me-3">
+                <a className="text-white fs-6 lh-sm fw-medium" href="#">
+                  商品系列
+                </a>
+              </li>
+              <li className="p-5 me-3">
+                <a className="text-white fs-6 lh-sm fw-medium" href="#">
+                  風格嚴選
+                </a>
+              </li>
+              <li className="p-5 me-3">
+                <a className="text-white fs-6 lh-sm fw-medium" href="#">
+                  關於收購
+                </a>
+              </li>
+              <li className="p-5 me-3">
+                <a className="text-white fs-6 lh-sm fw-medium" href="#">
+                  品牌故事
+                </a>
+              </li>
+              <li className="p-5">
+                <a className="text-white fs-6 lh-sm fw-medium" href="#">
+                  聯絡我們
+                </a>
+              </li>
+            </ul>
+            <div className="d-flex">
+              <div className="icon-wrapper p-5">
+                <div
+                  className="bg-transparent border-0 shadow-none p-0"
+                  role="button"
+                >
+                  <i className="bi bi-search text-white nav-icon"></i>
                 </div>
-            </nav>
-
-            {/* 電腦版導覽列 */}
-            <nav className="navbar navbar-expand-lg navbar-light bg-light d-none d-lg-flex fixed-top">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                    <div className="collapse navbar-collapse">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item"><a className="nav-link active" href="#">Home</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">Link</a></li>
-                        </ul>
-                        <form className="d-flex">
-                            <input className="form-control me-2" type="search" placeholder="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                    </div>
+              </div>
+              <div className="icon-wrapper p-5">
+                <div
+                  className="bg-transparent border-0 shadow-none p-0"
+                  role="button"
+                >
+                  <i className="bi bi-person text-white nav-icon"></i>
                 </div>
-            </nav>
-
-        </>
-    );
+              </div>
+              <div className="icon-wrapper p-5">
+                <div
+                  className="bg-transparent border-0 shadow-none p-0"
+                  role="button"
+                >
+                  <i className="bi bi-cart text-white nav-icon"></i>
+                  <span
+                    className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+                    style={{ fontSize: '0.7rem' }}
+                  >
+                    1
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
