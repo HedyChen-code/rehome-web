72b605a2b71787ea2f0a83186a91906f92bc0741
jiamori <lifesunny719@gmail.com>
2026-03-02 19:14:42 +0800

    修正跳轉頁面後灰層、購物車增減器

--- src/views/front/Cart.jsx
+++ src/views/front/Cart.jsx
@@ -10,7 +10,6 @@ const API_PATH = import.meta.env.VITE_API_PATH;
 
 const Cart = () => {
   const [ cart, setCart ] = useState({ cart: [] });
-  
   const navigate = useNavigate();
 
   const getCart = async () => {
@@ -25,7 +24,7 @@ const Cart = () => {
     }
   }
 
-  const updateCart = async (cartId, productId, qty=1) => {
+  const updateCartNum = async (cartId, productId, qty=1) => {
     try {
       const url = `${API_BASE}/api/${API_PATH}/cart/${cartId}`;
       const data = {
@@ -80,7 +79,7 @@ const Cart = () => {
     { !cart ? (
       <div className="text-center mt-5">載入中...</div>
     ) : (<>
-      <div className="bg-light-grey py-8 py-md-12">
+      <div className="bg-light-gray py-8 py-md-12">
         <Toaster position="top-right" reverseOrder={false} />
         <div className="container text-gray-95" style={{ marginTop: '144px' }}>
           <div className="row justify-content-center">
@@ -126,18 +125,35 @@ const Cart = () => {
                               </td>
                               <td>{ cartItem.product.title }</td>
                               <td>
-                                <div className="input-group mb-3">
-                                  <input 
-                                    type="number" 
-                                    className="form-control" 
-                                    placeholder="cart-product-num" 
-                                    aria-label="cart-product-num" 
-                                    aria-describedby="cart-product-num" 
-                                    defaultValue={ cartItem.qty }
-                                    onChange={ (e) => updateCart(cartItem.id, cartItem.product_id, Number(e.target.value)) }
-                                  />
-                                  <span className="input-group-text" id="cart-product-num">
-                                    { cartItem.product.unit }
+                                <div className="d-flex align-items-center gap-2">
+                                  <div className="input-group me-3 w-auto">
+                                    <button 
+                                      className="btn btn-outline-danger"
+                                      disabled={cartItem.qty <= 1}
+                                      onMouseEnter={ (e) => e.target.style.color = "#f8f9fa" }
+                                      onMouseLeave={ (e) => e.target.style.color = "" }
+                                      onClick={() => updateCartNum(cartItem.id, cartItem.product_id, cartItem.qty - 1)}
+                                    ><i className="bi bi-dash-circle"></i></button>
+                                    <input 
+                                      type="text" 
+                                      inputMode="numeric"
+                                      pattern="[0-9]*"
+                                      className="form-control text-center bg-white"
+                                      style={{flex: '0 0 70px'}} 
+                                      aria-label="cart-product-num" 
+                                      aria-describedby="cart-product-num" 
+                                      value={ cartItem.qty }
+                                      readOnly
+                                    />
+                                    <button 
+                                      className="btn btn-outline-danger"
+                                      onMouseEnter={ (e) => e.target.style.color = "#f8f9fa" }
+                                      onMouseLeave={ (e) => e.target.style.color = "" }
+                                      onClick={() => updateCartNum(cartItem.id, cartItem.product_id, cartItem.qty + 1)}
+                                    ><i className="bi bi-plus-circle"></i></button>
+                                  </div>
+                                  <span className="" id="cart-product-num">
+                                    / { cartItem.product.unit }
                                   </span>
                                 </div>
                               </td>
